7e5a9b24397d268603017de4b8c7b002f91e294c
rjsykes28@outlook.com <rjsykes28@outlook.com>
2022-05-31 16:42:51 -0500

    Completed Sections 13.1-13.3

---
+++ The Online Web Developer's Bootcamp/Sec13/scripts.js
@@ -0,0 +1,9 @@
+/* Section 13.8 - Attributes*/
+/* Section 13.7 - Style*/
+/* Section 13.6 - Text*/
+/* Section 13.5 - Selecting elements part 2*/
+/* Section 13.4 - Selecting elements part 1*/
+
+/* Section 13.3 - The window object*/
+/* Section 13.2 - Nodes*/
+/* Section 13.1 - What is the DOM?*/
